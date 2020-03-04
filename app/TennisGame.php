d3231b65fb081a6da0db7f26fa5e5620a646e06c
linsheng24 <linsheng24@gmail.com>
2020-03-05 02:16:56 +0800

    Pass LoveAll test

--- app/TennisGame.php
+++ app/TennisGame.php
@@ -3,39 +3,15 @@ namespace App;
 
 class TennisGame
 {
-  public function getGameScore($p1_score, $p2_score)
+ 
+  private $p1_score = 0;
+  private $p2_score = 0;
+
+  public function score()
   {
-   
-    if ($p1_score == $p2_score) {
-      if ($p1_score == 4) {
-        return 'Deuce';
-      }
-      return $this->getTextFromScore($p1_score) . '-All';
+    if ($this->p1_score == 0 && $this->p2_score == 0) {
+      return 'Love-All';
     }
-
-    return $this->getTextFromScore($p1_score) . '-' . $this->getTextFromScore($p2_score);
-     
   }
-  
-  private function getTextFromScore($score)
-  {
-    switch ($score) {
-      case 0:
-        $result = 'Love';
-        break;
-      case 1:
-        $result = 'Fifteen';
-        break;
-      case 2:
-        $result = 'Thirty';
-        break;
-      case 3:
-        $result = 'Forty';
-        break;
-    }
-    return $result;
-  }
-
-
 
 }
--- tests/TennisGameTest.php
+++ tests/TennisGameTest.php
@@ -8,169 +8,25 @@ class TennisGameTest extends TestCase
 {
   protected function setUp():void
   {
-      parent::setUp();
-      
-      $this->game = new TennisGame();
+    parent::setUp();
+    $this->game = new TennisGame();
   }
 
   protected function tearDown():void
   {
-      parent::tearDown();
-      
+    $this->game = null;
   }
-  
+
   /**
    * @test
    */
-  public function getGameScore_Give0vs0_ReturnLoveAll()
+  public function test_love_all()
   {
     //Arrange
-    $p1_score = 0;
-    $p2_score = 0;
-
     $expected = 'Love-All';
-
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-
-    //Assert
-    $this->assertEquals($expected, $actual);
-
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give1vs0_ReturnFifteenLove()
-  {
-    //Arrange
-    $p1_score = 1;
-    $p2_score = 0;
-
-    $expected = 'Fifteen-Love';
-
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-
-    //Assert
-    $this->assertEquals($expected, $actual);
-
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give0vs1_ReturnLoveFifteen()
-  {
-    //Arrange
-    $p1_score = 0;
-    $p2_score = 1;
-
-    $expected = 'Love-Fifteen';
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
     
-    //Assert
-    $this->assertEquals($expected, $actual);
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give0vs2_ReturnLoveThirty()
-  {
-    //Arrange
-    $p1_score = 0;
-    $p2_score = 2;
-
-    $expected = 'Love-Thirty';
     //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-    
-    //Assert
-    $this->assertEquals($expected, $actual);
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give1vs3_ReturnFifteenForty()
-  {
-    //Arrange
-    $p1_score = 1;
-    $p2_score = 3;
-    
-    $expected = 'Fifteen-Forty';
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-    
-    //Assert
-    $this->assertEquals($expected, $actual);
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give1vs1_ReturnFifteenAll()
-  {
-    //Arrange
-    $p1_score = 1;
-    $p2_score = 1;
-
-    $expected = 'Fifteen-All';
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-    
-    //Assert
-    $this->assertEquals($expected, $actual);
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give2vs2_ReturnThirtyAll()
-  {
-    //Arrange
-    $p1_score = 2;
-    $p2_score = 2;
-
-    $expected = 'Thirty-All';
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-    
-    //Assert
-    $this->assertEquals($expected, $actual);
-  }
-
-  /**
-   * @test
-   */
-  public function getGameScore_Give3vs3_ReturnFortyAll()
-  {
-    //Arrange
-    $p1_score = 3;
-    $p2_score = 3;
-    
-    $expected = 'Forty-All';
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
-    
-    //Assert
-    $this->assertEquals($expected, $actual);
-  }
-  
-  /**
-   * @test
-   */
-  public function getGameScore_Give4vs4_ReturnDeuce()
-  {
-    //Arrange
-    $p1_score = 4;
-    $p2_score = 4;
-    
-    $expected = 'Deuce';
-    //Act
-    $actual = $this->game->getGameScore($p1_score, $p2_score);
+    $actual = $this->game->score();
     
     //Assert
     $this->assertEquals($expected, $actual);
